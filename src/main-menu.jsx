559a1e4e4c79c30a0ed615c33202e26c43aa0209
nicole trinity <nimble.embodiment.8@gmail.com>
2025-06-28 22:30:20 -0400

    nits

--- src/main-menu.jsx
+++ src/main-menu.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const options = [
   { path: '/playcomputer', label: 'play computer' },
   { path: '/challenge', label: 'create challenge link' },
-  { path: '/leaderboard', label: 'leaderboard' },
 ];
 
 const MainMenu = () => {
@@ -24,19 +23,6 @@ const MainMenu = () => {
   return (
     <div>
       <h1>Fight2!</h1>
-      {user ? (
-        <div>
-          <p>Welcome, {user.displayName}!</p>
-          <p>
-            <a href="/logout">logout</a>
-          </p>
-          <p>
-            <a href="/myrecord">my record</a>
-          </p>
-        </div>
-      ) : (
-        <p><a href="/auth/google">Login with Google</a></p>
-      )}
       {options.map((option) => (
         <div key={option.path} className="option">
           <p><Link to={option.path}>{option.label}</Link></p>
